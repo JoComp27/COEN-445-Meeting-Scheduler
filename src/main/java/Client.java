496697607fee1823feaf2fc7db6da4053a4f13a4
Jonathan <jonathan.cournoyer@scigilian.com>
2019-10-23 19:27:45 -0400

    Started Client Code

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,3 +1,5 @@
+import requests.RequestType;
+
 import java.net.*;
 import java.io.*;
 import java.lang.*;
@@ -40,25 +42,85 @@ public class Client{
 
     public void run() {
 
+        //Create thread to listen to messages
+        new Thread(new ClientListen(serverAddress)).start();
 
+        while(true){
+            // INSERT UI FOR WHAT USER WANTS TO DO
 
-        // INSERT UI FOR WHAT USER WANTS TO DO
-
-        //SENDING A REQUEST MESSAGE
-
-
+            //SENDING A REQUEST MESSAGE
+        }
 
         //
 
     }
 
+    public class ClientListen implements Runnable {
 
+        InetAddress serverAddress;
+
+        public ClientListen(InetAddress serverAddress){
+            this.serverAddress = serverAddress;
+        }
+
+        @Override
+        public void run() {
+
+            //Like server, will listen to ip
+            /**Create new server and binds to a free port. From source of the internet
+             * the range should be 49152 - 65535.*/
+
+            /**The port address is chosen randomly*/
+            try(DatagramSocket serverSocket = new DatagramSocket(9999)) {
+                byte[] buffer = new byte[65535];
+                /**Messages here and sends to client*/
+                while(true){
+                    DatagramPacket DpReceive = new DatagramPacket(buffer, buffer.length);   //Create Datapacket to receive the data
+                    serverSocket.receive(DpReceive);        //Receive Data in Buffer
+                    String message = new String(DpReceive.getData());
+                    System.out.println("Server says: " + message);
+
+                    /**NEED TO ADD IN TIMEOUT OPTIONS TO RESEND THE MESSAGE. HAVE YET TO
+                     * COMPLETE THIS PORTION OF THE CODE
+                     *
+                     * Add in Thread and feed in the message*/
+                    //This would be the thread managing method
+                    new Thread(new ClientHandle(message)).start();
+
+                }
+
+            }catch (SocketException e){
+                e.printStackTrace();
+            }catch(IOException e){
+                e.printStackTrace();
+            }
+
+        }
+    }
 
     //Class for when the Client receives a message
     public class ClientHandle implements Runnable {
 
+        String message;
+
+        public ClientHandle(String message){
+            this.message = message;
+        }
+
         @Override
         public void run() {
+
+            String[] receivedMessage = message.split("_");
+            int messageType = Integer.parseInt(receivedMessage[0]);
+            RequestType receivedRequestType = RequestType.values()[messageType];
+
+            switch(receivedRequestType){
+                case Request :
+
+                    break;
+
+
+            }
 
         }
 
