f399c49e4d4985c26b89a4fac132f57d0c5c4bc3
Unknown <jonathan.cournoyer@scigilian.com>
2019-11-07 12:07:59 -0500

    Added back previous changes

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,4 +1,5 @@
-import requests.RequestType;
+import Tools.UdpSend;
+import requests.*;
 
 import java.net.*;
 import java.util.Scanner;
@@ -7,53 +8,31 @@ import java.lang.*;
 
 public class Client{
 
-    private InetAddress serverAddress;
-    private InetAddress selfAddress;
-
-    private DatagramSocket ds;
-
     private int requestNumber;
 
-    public Client(String serverAddress){
+
+    public Client(){
+
         this.requestNumber = 0;
 
     }
 
     public static void main(String args[]) throws IOException {
-
-        if(args.length == 0){
-            System.out.println("Server IP is missing");
-            return;
-        }
-        Client client = new Client(args[0]);
+        Client client = new Client();
         client.run();
     }
 
-    private void sendMessageToServer(String message) throws IOException {
-
-        // convert the String input into the byte array.
-        byte buf[] = message.getBytes();
-        byte[] buffer = new byte[100];
-        DatagramPacket DpSend = new DatagramPacket(buf, buf.length, serverAddress, 9999);
-
-        ds.send(DpSend);
-        System.out.println("MESSAGE SENT");
-
-        DatagramPacket DpReceive = new DatagramPacket(buffer, buffer.length);   //Create Datapacket to receive the data
-        ds.receive(DpReceive);        //Receive Data in Buffer
-        String messageFromServer = new String(DpReceive.getData());
-        System.out.println("Server says: " + messageFromServer);
-
-    }
     public void run() throws IOException {
 
         Scanner sc = new Scanner(System.in);
+
         // loop while user not enters "bye"
         while (true)
         {
             String inp = sc.nextLine();
 
-            sendMessageToServer(inp);
+            UdpSend.sendMessage(inp, 9999);
+
             // break the loop if user enters "bye"
             if (inp.equals("bye"))
                 break;
@@ -70,6 +49,40 @@ public class Client{
 //        }
 //
 //        //
+
+    }
+
+    private void handleDenied(DeniedMessage message) {
+
+
+
+    }
+
+    private void handleInvite(InviteMessage message) {
+
+    }
+
+    private void handleConfirm(ConfirmMessage message){
+
+    }
+
+    private void handleServerCancel(ServerCancelMessage message){
+
+    }
+
+    private void handleScheduled(ScheduledMessage message){
+
+    }
+
+    private void handleNotSchedules(NotScheduledMessage message) {
+
+    }
+
+    private void handleAdded(AddedMessage message){
+
+    }
+
+    private void handleRoomChange(RoomChangeMessage message) {
 
     }
 
@@ -97,6 +110,7 @@ public class Client{
                     serverSocket.receive(DpReceive);        //Receive Data in Buffer
                     String message = new String(DpReceive.getData());
                     System.out.println("Server says: " + message);
+
                     /**NEED TO ADD IN TIMEOUT OPTIONS TO RESEND THE MESSAGE. HAVE YET TO
                      * COMPLETE THIS PORTION OF THE CODE
                      *
@@ -132,10 +146,32 @@ public class Client{
             RequestType receivedRequestType = RequestType.values()[messageType];
 
             switch(receivedRequestType){
-                case Request :
+                case Denied :
+                    DeniedMessage deniedMessage = new DeniedMessage();
+                    deniedMessage.deserialize(message);
+                    handleDenied(deniedMessage);
+                    break;
+                case Invite :
 
                     break;
+                case Confirm :
 
+                    break;
+                case ServerCancel :
+
+                    break;
+                case Scheduled :
+
+                    break;
+                case NotScheduled :
+
+                    break;
+                case Added :
+
+                    break;
+                case RoomChange :
+
+                    break;
 
             }
 
@@ -144,3 +180,4 @@ public class Client{
     }
 
 }
+
